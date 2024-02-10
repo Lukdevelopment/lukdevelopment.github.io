86141f47c5596b233ab13299112bf125462d1864
Lunanaul <81565314+Lunanaul@users.noreply.github.com>
2024-02-10 10:54:01 +0100

    Update init.sh

--- something/init.sh
+++ something/init.sh
@@ -5,7 +5,7 @@ mkdir ./system
 touch ./system/wget.txt
 touch ./system/pip_install.txt
 wget https://lukdev.org/something/requirements.txt -P ./system -a ./system/wget.txt
-wget https://lukdev.org/something/inventory.ini -P ./ansible -a ./system/wget.txt
-wget https://lukdev.org/something/playbook.yaml -P ./ansible -a ./system/wget.txt
+wget https://lukdev.org/something/inventory.ini -a ./system/wget.txt
+wget https://lukdev.org/something/playbook.yaml -a ./system/wget.txt
 pip install -r ./system/requirements.txt > ./system/pip_install.txt
-ansible-playbook -i ./ansible/inventory.ini ./ansible/playbook.yaml
+ansible-playbook -i inventory.ini playbook.yaml
