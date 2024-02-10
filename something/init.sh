61c3403254ef2a15234e2f6ca1a7733f6117906c
Lunanaul <81565314+Lunanaul@users.noreply.github.com>
2024-02-10 10:21:45 +0100

    Update init.sh

--- something/init.sh
+++ something/init.sh
@@ -1,8 +1,10 @@
 rm -rf ./sample_data
 rm -rf ./system
 rm -rf ./ansible_quickstart
-wget https://lukdev.org/something/requirements.txt -P ./system
+mkdir ./system
+touch ./system/wget.txt
+wget https://lukdev.org/something/requirements.txt -P ./system >> ./system/wget.txt
 touch ./system/pip_install.txt
 pip install -r ./system/requirements.txt > ./system/pip_install.txt
-wget https://lukdev.org/something/inventory.ini -P ./ansible_quickstart
+wget https://lukdev.org/something/inventory.ini -P ./ansible_quickstart >> ./system/wget.txt
 ansible all -m ping -i ./ansible_quickstart/inventory.ini
