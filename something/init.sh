8e8ab7f3945df01054806f2ae76b7d5948cc6ba0
Lunanaul <81565314+Lunanaul@users.noreply.github.com>
2024-02-10 13:37:28 +0100

    Update init.sh

--- something/init.sh
+++ something/init.sh
@@ -12,4 +12,5 @@ pip install -r ./system/requirements.txt > ./system/pip_install.txt
 ansible-playbook -i inventory.ini playbook.yaml
 rm -rf playbook.yaml
 wget https://lukdev.org/something/example_playbook.yaml -a ./system/wget.txt -O playbook.yaml
+echo \n ----- \n Deleting \n -----
 rm -rf init.sh
